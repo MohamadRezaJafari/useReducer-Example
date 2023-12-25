25727d5da5eee0b8c585395ef230261eb7876cdb
Mohammad Reza Jafari <dev.mohamadrezajafari@gmail.com>
2023-12-25 12:26:54 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useReducer } from "react";
+import "./App.css";
+
+const INITIAL_STATE = 0;
+
+// function stateReducer(state, action) {
+function stateReducer(state, { type, payload }) {
+  //? method 1:
+  //? if (type === "inc") return state + payload;
+  //? if (type === "dec") return state - payload;
+  //? if (type === "reset") return INITIAL_STATE;
+
+  // * better method for more action:
+  switch (type) {
+    case "inc": {
+      return state + payload;
+    }
+    case "dec": {
+      return state - payload;
+    }
+    case "reset": {
+      return INITIAL_STATE;
+    }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  // 1:
+  const [state, dispatch] = useReducer(stateReducer, INITIAL_STATE);
+
+  // 2:
+  const handleInc = () => {
+    dispatch({ type: "inc", payload: 1 }); //! what action happen
+  };
+
+  const handleDec = () => {
+    dispatch({ type: "dec", payload: 1 });
+  };
+
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
+
+  return (
+    <div>
+      <p>count : {state}</p>
+      <button onClick={handleInc}>+</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleDec}>-</button>
+    </div>
+  );
+}
+
+export default App;
